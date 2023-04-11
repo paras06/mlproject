dab5095057d179f6b1f21a98496461ea8df45a16
paras06 <paras06@gmail.com>
2023-04-11 13:55:17 +0100

    logging and excption files created

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-
+import logging
 
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tab = error_detail.exc_info()
@@ -12,9 +12,11 @@ def error_message_detail(error, error_detail: sys):
 
 class Custom_Exception(Exception):
     def __init__(self, error_message, error_detail: sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message = error_message_detail(
             error_message, error_detail=error_detail)
 
     def __str__(self):
         return self.error_message
+
+    
