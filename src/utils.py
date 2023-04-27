d07c19fb886a72eb2ba9b83b26dc12114a6f2ba2
paras06 <paras06@gmail.com>
2023-04-27 16:15:52 +0100

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 import dill
 
 from src.exception import Custom_Exception
+from sklearn.model_selection import train_test_split
 
 def save_object(file_path, obj):
     try:
@@ -17,3 +18,25 @@ def save_object(file_path, obj):
     
     except Exception as e:
         raise Custom_Exception(e, sys)
+    
+def evaluate_model(X,y,models):
+    try:
+        X_train, X_test, y_train, y_test = train_test_split(
+            X,y,test_size=0.2,random_state=42
+        )
+
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+
+            model.fit(X_train, y_train) # Train Model
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            
+
+
+    except Exception as e:
+        raise Custom_Exception(e,sys)
